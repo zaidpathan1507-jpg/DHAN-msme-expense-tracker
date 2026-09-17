ddac58cf0b9a485d9b8da72ae7c93d0510daf8d6
zaidpathan15 <zaidpathan1507@gmail.com>
2026-09-18 01:44:19 +0530

    Configure DHAN for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,18 @@
 import { defineConfig } from 'vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import netlify from '@netlify/vite-plugin-tanstack-start'
+import { nitro } from 'nitro/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 
-const config = defineConfig({
+export default defineConfig({
   plugins: [
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
     }),
     tailwindcss(),
     tanstackStart(),
+    nitro(),
     viteReact(),
-    netlify(),
   ],
 })
-
-export default config
